5c29d064bb5bcbe3e20bb0419fd712c729dfa6c1
NirvanaPayments <83754486+NirvanaPayments@users.noreply.github.com>
2022-01-15 08:08:52 +0100

    Update script.bash

--- communication_python/script.bash
+++ communication_python/script.bash
@@ -4,7 +4,7 @@ for i in {100..100000..10000}
 do
     python3 NirvanaAuthorities.py $i  & disown
     sleep 2
-    python3 Merchant.py & disown
+    python3 Merchant_witness_distributed.py & disown
     sleep 2
     python3 Customer_preprocessed.py &disown
     sleep 2
